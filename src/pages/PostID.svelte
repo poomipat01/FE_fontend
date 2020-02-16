8d76625f671cde05b4712d276e3a778d40453ba6
poomipat01 <poomipat002@gmail.com>
2020-02-16 14:47:00 +0700

    first commit

---
+++ src/pages/PostID.svelte
@@ -0,0 +1,55 @@
+<script>
+  import { pages } from "../store.js";
+  import { fade } from "svelte/transition";
+  import ReplyList from "../components/ReplyList.svelte";
+  import ReplyListItem from "../components/ReplyListItem.svelte";
+  let topic;
+  const fetchPostbyid = async () => {
+    let id = $pages.replace("PostID?id=", "");
+    let res = await fetch(`http://localhost:1234/api/posts/${id}`);
+    let data = await res.json();
+    if (res.ok) {
+      topic = data.topic;
+      return data;
+    } else {
+      let err = await res.text();
+      throw new Error(err);
+    }
+  };
+  let onReply = () => {
+    promise = fetchPostbyid();
+  };
+  let promise = fetchPostbyid();
+</script>
+
+<style>
+
+</style>
+
+<svelte:head>
+  {#if topic}
+    <title>Post: {topic}</title>
+  {/if}
+</svelte:head>
+
+<section>
+  <!-- รอโหลดอยู่นิ -->
+  {#await promise}
+    <p>Fetching posts . . .</p>
+  {:then post}
+    <!-- ละถ้าไม่มีใครโพสกระทู้ล่ะ -->
+    {#if post}
+      <ReplyList
+        topic={post.topic}
+        name={post.name}
+        content={post.content}
+        on:ReplySuccess={onReply}>
+        {#each post.comment as comment} <!-- ชื่อไรดี -->
+          <ReplyListItem name={comment.name} reply={comment.reply} />
+        {/each}
+      </ReplyList>
+    {/if}
+  {:catch}
+    <div>Error</div>
+  {/await}
+</section>
